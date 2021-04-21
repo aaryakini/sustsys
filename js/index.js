27300d68ff4ebc0b04b1db882e1955e7380de7a2
aaryakini <kinia313@newschool.edu>
2021-04-22 07:58:38 +0800

    Updates to index + javascript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function swapImage(id,primary,secondary)
+{
+    src = document.getElementById(id).src;
+    if (src.match(primary)) {
+      document.getElementById(id).src=secondary;
+    } else {
+      document.getElementById(id).src=primary;
+    }
+}

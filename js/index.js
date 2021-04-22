8b554e0a50c08fb9f5294279215bef063ffda5d8
aaryakini <kinia313@newschool.edu>
2021-04-22 08:02:25 +0800

    Adding citation to index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+/*
+*    Title: Swapping images by mouseclick using Jvascript
+*    Author: Dr. Rudolf Winter
+*    Date: 9/11/2013
+*    Code version: Source Code
+*    Availability: https://users.aber.ac.uk/ruw/misc/swapimage.php
+*
+*/
+
 function swapImage(id,primary,secondary)
 {
     src = document.getElementById(id).src;

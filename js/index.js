599ff71bf0abe2230fdc85e159eb83e1139e22c6
aaryakini <kinia313@newschool.edu>
2021-04-29 00:32:37 +0800

    many many updates to everything

--- js/index.js
+++ js/index.js
@@ -16,3 +16,24 @@ function swapImage(id,primary,secondary)
       document.getElementById(id).src=primary;
     }
 }
+
+/*
+*    Title: How to Collapse Sidebar
+*    Author: W3 Schools
+*    Date: Accessed 26th April 2021
+*    Code version: Source Code
+*    Availability: https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp
+*
+*/
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
